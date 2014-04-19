6b4cd9301af3fbc331bb107e2c60b96be57c3bfb
Royceb <rbeezy707@gmail.com>
2014-04-18 18:16:02 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+MyE3::Application.config.secret_key_base = '5a4bef873be100fa1164c60457c0b1d4b6916ca4ac3821a823c97febf63a5155032d491662563438d648e6103c7d239fe4dc392a213d8a25910db39199fc60d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyE3::Application.config.session_store :cookie_store, key: '_myE3_session'
